7ec8d582301c6c6a1cc8c4e32a8a605063239094
adamtschannen <adamtschannen@googlemail.com>
2017-06-27 00:05:37 -0700

    need to correct

--- BrewsBeersCode.R
+++ BrewsBeersCode.R
@@ -3,8 +3,52 @@
 brew<- read.csv(file="~/Downloads/Breweries_CaseStudy1.csv")
 beer<- read.csv(file="~/Downloads/Beers_CaseStudy1.csv")
 
+#part1
 #count number of breweries in each state
 #plot for visualization 
 BrewPerState <- table(brew$State)
 plot(BrewPerState,ylab = "number of breweries", ylim=c(0,50))
+
+#part 2
+# change the column name Brew_id in Breweries_CaseStudy1 data set
+# to match the column name Brewery_id in the Beers_CaseStudy1 data
+# set
+names(brew)[1]<-"Brewery_id"    #option 1
+names(brew) <- c("Brewery_id")  #option 2 
+
+#part 2a
+#joining the two data sets
+#need library plyr for this code
+library(plyr)
+brewbeer <- join(x=brew, y=beer, by= 'Brewery_id')
+
+#part 2b
+#printing out first and last 6 obs in the new merged data set
+head(brewbeer, n=6)
+tail(brewbeer, n=6)
+
+#part 3- reportt the number of na'sin each column 
+sapply(brewbeer, function(x) sum(is.na(x)))
+
+#part 4 -Compute the median alcohol content and international
+#bitterness unit for each state. Plot a bar chart to compare.
+
+MedianABVperState<-ggplot(brewbeer, aes(x=factor(State), y=ABV)) + stat_summary(fun.y="median", geom="bar", rm.na=TRUE)
+MedianIBUPerState<-ggplot(brewbeer, aes(x=factor(State), y=IBU)) + stat_summary(fun.y="median", geom="bar", rm.na=TRUE)
+
+#part 5 -Which state has the maximum alcoholic beer? Which state has the most bitter beer?
+alcmax<-max(brewbeer$ABV, na.rm = TRUE) #na.rm=TRUE removes na values 
+IBUmax<-max(brewbeer$IBU, na.rm = TRUE)
+
+
+maxalc<-sort(StateMaxABV$x , decreasing = FALSE, is.na(StateMaxABV$x))
+tail(maxalc)
+maxalc2<-sort(StateMaxABV$Group.1 , decreasing = FALSE)
+
+
+StateMaxABV<-aggregate(brewbeer$ABV, by = list(brewbeer$State), max, is.na(brewbeer$x))
+
+StateMaxABV1 <- StateMaxABV[,order('State')] 
+
+
 
