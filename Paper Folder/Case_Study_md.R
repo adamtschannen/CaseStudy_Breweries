2285092eae37a364bd57617e354cc2f3d05efc5b
adamtschannen <adamtschannen@googlemail.com>
2017-06-30 17:43:48 -0700

    Update Case_Study_md.R

--- Paper Folder/Case_Study_md.R
+++ Paper Folder/Case_Study_md.R
@@ -133,6 +133,16 @@ alcbitcor<-cor(brewbeer$ABV, brewbeer$IBU,use = "pairwise.complete.obs", method 
 #find confidence intervals and test of the correlation 
 alcbitcortest<-cor.test(brewbeer$ABV, brewbeer$IBU,use = "pairwise.complete.obs", method = "pearson")
 ```
-
+ In conclusion, we found that Colorado was the State with the most breweries with
+47 and DC, North Dakota, South Dakota and West Virginia were tied for the 
+State with the least number of breweries. We found that the range of median 
+alcohol by volume is between 4% and 6.25% while the range of median International
+Bitterness Unit is between 19 and 61. The state with the max alcohol by volume 
+is Colorado with 12.8% while the state with the max International Bitterness Unit
+was Oregon with 138. Furthermore,  we found a summary of the alcohol by volume and that 
+the min was %.1 the first quartile was  %.5000 the median is %5.6 the mean is  
+%5.98 the third quartile %6.7 and the max is  %12.8. Finally, we found that 
+there is a relatively strong correlation of .67 between alcohol by volume and the 
+bitterness of beer of which is statistically significant with confidence intervals between  (0.6407982, 0.6984238).
 
 
