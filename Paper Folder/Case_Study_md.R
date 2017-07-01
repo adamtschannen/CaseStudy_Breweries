d6e72c23eb883b5e11d4d19597063ae87b64b197
adamtschannen <adamtschannen@googlemail.com>
2017-06-30 17:41:19 -0700

    Update Case_Study_md.R

--- Paper Folder/Case_Study_md.R
+++ Paper Folder/Case_Study_md.R
@@ -6,14 +6,18 @@ output: html_document
 ---
 
 
-With the data sets of Beers and Breweries we analyzed and found the number of breweries in each state, the median alcohol content and international bitterness unit for each state, which state has the maximum alcoholic beer, which state has the most bitter beer, a summary of the Alcohol by volume and the relationship between the bitterness of the beer and its alcoholic content? 
+With the data sets of Beers and Breweries we analyzed and found the number of breweries in each state, the median alcohol
+content and international bitterness unit for each state, which state has the maximum alcoholic beer, which state has the 
+most bitter beer, a summary of the Alcohol by volume and the relationship between the bitterness of the beer and its alcoholic
+content.
 ```{r}
 # read in csv files 
 brew<- read.csv(file="~/Downloads/Breweries_CaseStudy1.csv")
 beer<- read.csv(file="~/Downloads/Beers_CaseStudy1.csv")
 ```
 
-Below we can see the number of breweries in each state in in ascending order with Colorado having the most number of breweries with 47 and DC, North Dakota, South Dakota and West Virginia having the least number of breweries with 1. 
+Below we can see the number of breweries in each state in in ascending order with Colorado having the most number of breweries
+with 47 and DC, North Dakota, South Dakota and West Virginia having the least number of breweries with 1. 
 
 ```{r}
 #part1
@@ -33,9 +37,12 @@ ID LA  NE  RI  OK  AK  GA  MD  CT  ME  MO  MT  VT  AZ  MN  FL  OH  NY  VA  IL  N
 WI IN  MA  WA  PA  TX  OR  MI  CA  CO 
 20 22  23  23  25  28  29  32  39  47
 
-Next, we merged the beer data with breweries data by brewery id. We did this so we would have all of our data on one sheet. To check that we executed this step correctly we printed first 6 observations and the last six observations to check the merged file.
+Next, we merged the beer data with breweries data by brewery id. We did this so we would have all of our data on one sheet.
+To check that we executed this step correctly we printed first 6 observations and the last six observations to check the 
+merged file.
 
-The first step in merging was making sure that both data sets had the same variable name 'Brewery_id' so that we could merge them. 
+The first step in merging was making sure that both data sets had the same variable name 'Brewery_id' so that we could merge 
+them. 
 ```{r}
 #part 2
 #this names the first column 'Brewery_id' so we can merge the files
@@ -115,7 +122,8 @@ summary(brewbeer$ABV)
   Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
 0.00100 0.05000 0.05600 0.05977 0.06700 0.12800
 
-To find the relationship between the bitterness of the beer and its alcoholic content we made a scatter plot and checked its correlation .
+To find the relationship between the bitterness of the beer and its alcoholic content we made a scatter plot and checked 
+its correlation.
 ```{r}
 #part 7
 #finding the relationship between ABV and IBU
